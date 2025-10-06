c1cce2a87b9e816e179dd8b24e765ca5184d1d03
YuDavidCao <yu.cao20041208@gmail.com>
2025-10-06 09:36:29 -0400

    more progress

--- mlpt.c
+++ mlpt.c
@@ -12,13 +12,30 @@
 #define PTE_SIZE 8
 #define PTE_BITS 3
 
-#define BIT_PER_TABLE (POBITS - PTE_BITS)
+#define BIT_PER_TABLE_LEVEL (POBITS - PTE_BITS)
 
 size_t ptbr = 0;
 size_t page_size = 0;
 
 int is_valid(size_t a) {
     return a & 1;
+}
+
+size_t construct_mask(size_t n_bits) {
+    size_t mask = 1;
+    for (size_t i = 0; i < n_bits - 1; i++) {
+        mask = (mask << 1) + (size_t)1;
+    }
+    return mask;
+}
+
+size_t extract_offset(size_t a) {
+    return a & construct_mask(POBITS);
+}
+
+size_t extract_vpn_part(size_t va, size_t level) {
+    size_t shift_amount = POBITS + ((LEVELS - 1 - level) * BIT_PER_TABLE_LEVEL);
+    return (va >> shift_amount) & construct_mask(BIT_PER_TABLE_LEVEL);
 }
 
 void page_table_init() {
@@ -39,6 +56,10 @@ void page_table_init() {
 int allocate_page(size_t va) {
     if (!ptbr) {
         page_table_init();
+    }
+
+    for (size_t i = 0; i < LEVELS; i++) {
+        size_t vpn = extract_vpn_part(va, i);
     }
 }
 
